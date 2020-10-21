719bc800d095a3f0ec2d04d8c0d515e4ffbee7f2
TakuyaNamba <biwako0418@gmail.com>
2020-10-21 15:39:37 +0900

    完成版です！

---
+++ Swift5IntroApp1/Model/NewsItems.swift
@@ -0,0 +1,14 @@
+//
+//  NewsItems.swift
+//  Swift5IntroApp1
+//
+//  Created by 難波 拓也 on 2020/10/21.
+//
+
+import Foundation
+
+class NewsItems {
+    var title:String?
+    var url:String?
+    var pubDate:String?
+}
